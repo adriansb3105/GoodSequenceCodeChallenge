38b9e78641574dab06914d39ecd9846b13883dcb
Adrian <adriansb3105@gmail.com>
2022-08-04 22:36:39 -0600

    Finish the code challenge

--- index.js
+++ index.js
@@ -1,15 +1,20 @@
+/**
+ * @author Adrian Serrano
+ * @date 04/08/2022
+ * @name Good Sequence Code Challenge
+ */
+
 function minCost(N, H, M) {
     let result = 0;
     
     for (let i = 1; i <= N; i++) {
         if (H[i-1] === H[i]) {
-            let min = Math.min(M[i-1], M[i]);
-            
-            result += 
-            console.log(result);
+            let minI = M[i-1] < M[i]? i-1 : i;
+            H[minI] += 1;
+            result += M[minI];
         }
     }
-    
+
     return result;
 }
 
@@ -21,20 +26,18 @@ let INPUT = `
 3   5       --denotes two integers Hi and Mi
 `;
 
-let temp = INPUT.trim('\n').split(/\n/);
+let temp = INPUT.trim().split(/\n/);
 let N = parseInt(temp[0]);
 
 let H = [];
-for (let i = 1; i < 1+N; i++) {
-    H.push(parseInt(temp[i]));
-}
-
 let M = [];
-for (let i = 1; i < 1+N; i++) {
-    let m = temp[i].replace(/ /g, '').split('');
+
+for (let i = 1; i <= N; i++) {
+    H.push(parseInt(temp[i]));
+
+    let m = temp[i].split(/\s|\t/).filter(s => s !== '');
     M.push(parseInt(m[1]));
 }
 
 // OUTPUT
-//console.log(minCost(N, H, M));
-minCost(N, H, M)
+console.log(minCost(N, H, M));
